17f4bd43ebb17a6e019bd6004f165a26d7dbb168
Thiago <t.moraisaraujo@gmail.com>
2021-02-27 12:23:04 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- lists/views.py
+++ lists/views.py
@@ -10,3 +10,8 @@ def home_page(request):
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
+
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
